f9e7efd8d50a0a33f5950c78028060a338a1b814
seafu47 <jiaheng.fang@sjsu.com>
2022-03-24 21:28:23 -0700

    changed

--- home.py
+++ home.py
@@ -2,27 +2,31 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
+name = "Lisa"
+city_names = ["Paris", "London", "Rome", "Tahiti"]
 
 @app.route("/")
 def home():
-	name = "Lisa"
-	city_names = ["Paris", "London", "Rome", "Tahiti"]
 	return '''
 	<html>
 	<head>
-		<title>home page</title>
-	</head>
+		<h1>Welcome '''+name+'''!</h1>
+	</head>	
+	<p><a href="www.google.com">not google</a></p>
+	
 	<body>
-		<h1>Welcome '''+name+'''</h1>		
-		<p> <a href=https://www.google.com/"> not google </a> </p>
+	
 	<ul>
 		<li>'''+city_names[0]+'''</li>
 		<li>'''+city_names[1]+'''</li>
 		<li>'''+city_names[2]+'''</li>
 		<li>'''+city_names[3]+'''</li>
+
 	</ul>
 	</body>
 	</html> '''
+
+def loop_
 	
 if __name__ == "__main__":
 	app.run()
